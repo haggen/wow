ccf6e5d76c6e778f0c925fece978bb32165883a8
Arthur Corenzan <arthur@corenzan.com>
2018-09-16 14:54:16 -0300

    Fix reset command (#2)

--- Focus/Focus.lua
+++ Focus/Focus.lua
@@ -1,44 +1,60 @@
 -- Focus 1.0.0
 -- The MIT License © 2017 Arthur Corenzan
 
--- Hook around a frame's script handler.
-local function HookScript(frame, script, hook)
-    local handler = frame:GetScript(script)
-    frame:SetScript(script, function()
-        hook(function()
-            if handler then
-                handler()
-            end
-        end)
-    end)
+-- Shortcut to print in chat.
+local function print(message)
+    DEFAULT_CHAT_FRAME:AddMessage(message);
 end
 
---
---
---
+-- Used to enable/disable dragging of PlayerFrame.
+local isPlayerFrameLocked;
 
--- Used during setup to rollback the changes
+-- Used to check if TargetFrame should be moving as well.
+local isPlayerFrameMoving;
+
+-- Used during to rollback any changes
 -- in case the player clicks on Discard.
-local savedPlayerFrameOffsetX
-local savedPlayerFrameOffsetY
-local savedTargetFrameOffsetX
-local savedTargetFrameOffsetY
+local savedPlayerFrameOffsetX;
+local savedPlayerFrameOffsetY;
+local savedTargetFrameOffsetX;
+local savedTargetFrameOffsetY;
 
-local function Focus_Setup()
-    -- First of all let's re-anchor both PlayerFrame and TargetFrame 
-    -- smackdab at the center of the screen. The first time it happens
-    -- both frames will be repositioned but on subsequential runs they won't.
-    -- That's because by then they'll have been flagged as "user placed"
-    -- and as such their position is handled by the layout cache.
-    PlayerFrame:ClearAllPoints()
-    PlayerFrame:SetPoint("RIGHT", nil, "CENTER", 0, 0)
+-- Run once after install and on the first "/focus" after a reset.
+local function Setup()
+    -- Save default position of both frames but drop the relativeFrame 
+    -- value since it's a frame can't be encoded in saved variables.
+    -- nil will default to UIParent which is the original value.
+    FocusSavedVars.defaultPlayerFramePoint = { PlayerFrame:GetPoint(1) };
+    FocusSavedVars.defaultTargetFramePoint = { TargetFrame:GetPoint(1) };
+    if (type(FocusSavedVars.defaultPlayerFramePoint[2]) == "userdata") then
+        FocusSavedVars.defaultPlayerFramePoint[2] = FocusSavedVars.defaultPlayerFramePoint[2]:GetName();
+    end
+    if (type(FocusSavedVars.defaultTargetFramePoint[2]) == "userdata") then
+        FocusSavedVars.defaultTargetFramePoint[2] = FocusSavedVars.defaultTargetFramePoint[2]:GetName();
+    end
 
-    TargetFrame:ClearAllPoints()
-    TargetFrame:SetPoint("LEFT", nil, "CENTER", 0, 0)
+    -- Move both PlayerFrame and TargetFrame smackdab at the center of the screen.
+    -- The first time it happens both frames will be moved but on subsequential 
+    -- runs they won't. That's because by then they'll have been flagged as "user placed"
+    -- and as such their position is handled by the layout cache of the game.
+    PlayerFrame:ClearAllPoints();
+    PlayerFrame:SetPoint("RIGHT", nil, "CENTER", 0, 0);
 
-    -- Flag both frames as "user placed". This makes the previous lines
-    -- void ofter the first run and makes the game remember the frame's
-    -- position after login.
+    TargetFrame:ClearAllPoints();
+    TargetFrame:SetPoint("LEFT", nil, "CENTER", 0, 0);
+
+    FocusSavedVars.setup = true;
+    FocusSavedVars.reset = nil;
+end
+
+-- Run on UI load as long as it's not been reset.
+local function Startup()
+    if (not FocusSavedVars.setup) then
+        Setup();
+    end
+
+    -- Flag both frames as "user placed". This makes the
+    -- game remember the frames position across sessions.
     PlayerFrame:SetUserPlaced(true)
     TargetFrame:SetUserPlaced(true)
 
@@ -49,117 +65,149 @@ local function Focus_Setup()
     -- can mirror its movement.
     PlayerFrame:RegisterForDrag("LeftButton")
     PlayerFrame:SetScript("OnDragStart", function()
-        if not this.isLocked then
-            this:StartMoving()
-            this.isMoving = true
+        if (not isPlayerFrameLocked) then
+            PlayerFrame:StartMoving();
+            isPlayerFrameMoving = true;
         end
-    end)
+    end);
     PlayerFrame:SetScript("OnDragStop", function()
-        if this.isMoving then
-            this:StopMovingOrSizing()
-            this.isMoving = nil
+        if (isPlayerFrameMoving) then
+            PlayerFrame:StopMovingOrSizing();
+            isPlayerFrameMoving = nil;
         end
-    end)
+    end);
 
-    -- Starts locked.
-    PlayerFrame.isLocked = true
-
-    -- Here we hook after the OnUpdate handler of TargetFrame and 
-    -- check if the PlayerFrame's being moved. If so we do some 
-    -- calcuation to mirror its movement across the Y axis.
-    HookScript(TargetFrame, "OnUpdate", function(originalHandler)
-        originalHandler()
-
-        if PlayerFrame.isMoving then
-            local _, _, _, offsetX, offsetY = PlayerFrame:GetPoint(1)
-            local mirroredOffsetX = GetScreenWidth() - offsetX - TargetFrame:GetWidth()
-
-            TargetFrame:ClearAllPoints()
-            TargetFrame:SetPoint("TOPLEFT", nil, "TOPLEFT", mirroredOffsetX, offsetY)
-        end    
-    end)
+    -- Unlocks with "/focus".
+    isPlayerFrameLocked = true;
 end
 
--- Reset both PlayerFrame and TargetFrame
--- back to the center of the screen.
--- Prompt before reloading the UI.
-local function Focus_Reset()
-    PlayerFrame:SetMovable(false)
-    TargetFrame:SetMovable(false)
+-- Undo changes made during setup
+-- then prompt to reload the UI.
+local function Reset()
+    PlayerFrame:SetUserPlaced(false);
+    TargetFrame:SetUserPlaced(false);
 
-    StaticPopup_Show("FOCUS_RESET")
+    PlayerFrame:ClearAllPoints();
+    PlayerFrame:SetPoint(unpack(FocusSavedVars.defaultPlayerFramePoint));
+
+    TargetFrame:ClearAllPoints();
+    TargetFrame:SetPoint(unpack(FocusSavedVars.defaultTargetFramePoint));
+    
+    PlayerFrame:SetScript("OnDragStart", nil);
+    PlayerFrame:SetScript("OnDragStop", nil);
+    
+    PlayerFrame:RegisterForDrag(nil);
+
+    FocusSavedVars.reset = true;
+    FocusSavedVars.setup = nil;
 end
 
 -- Unlock PlayerFrame for moving. Save both PlayerFrame
 -- and TargetFrame positions in case of rollback, and then
 -- prompt for an interface reload.
-local function Focus_Unlock()
-    if not UnitExists("target") then
-        TargetUnit("player")
+local function Unlock()
+    -- Make sure the player is targeting something so 
+    -- they can see the TargetFrame moving as well.
+    if (not UnitExists("target")) then
+        TargetUnit("player");
     end
 
-    _, _, _, savedPlayerFrameOffsetX, savedPlayerFrameOffsetY = PlayerFrame:GetPoint(1)
-    _, _, _, savedTargetFrameOffsetX, savedTargetFrameOffsetY = TargetFrame:GetPoint(1)
+    -- Save momentarily the current offsets.
+    _, _, _, savedPlayerFrameOffsetX, savedPlayerFrameOffsetY = PlayerFrame:GetPoint(1);
+    _, _, _, savedTargetFrameOffsetX, savedTargetFrameOffsetY = TargetFrame:GetPoint(1);
 
-    PlayerFrame.isLocked = false
-    StaticPopup_Show("FOCUS_SETUP")
+    -- Unlock dragging for PlayerFrame.
+    isPlayerFrameLocked = false;
+
+    StaticPopup_Show("FOCUS_SETUP");
 end
 
 -- Lock PlayerFrame for moving and hide the prompt.
-local function Focus_Relock()
-    PlayerFrame.isLocked = true
-    StaticPopup_Hide("FOCUS_SETUP")
+local function Relock()
+    isPlayerFrameLocked = true;
+    StaticPopup_Hide("FOCUS_SETUP");
 end
 
 -- Called when the player clicks Apply in the prompt.
-local function Focus_Apply()
-    Focus_Relock()
+local function Apply()
+    -- Simply relock since its position is 
+    -- already being tracked by layout cache.
+    Relock()
 end
 
 -- Called when the player clicks Discard in the prompt.
-local function Focus_Discard()
+local function Discard()
+    -- Restore saved position.
     PlayerFrame:ClearAllPoints()
     PlayerFrame:SetPoint("TOPLEFT", nil, "TOPLEFT", savedPlayerFrameOffsetX, savedPlayerFrameOffsetY)
 
     TargetFrame:ClearAllPoints()
     TargetFrame:SetPoint("TOPLEFT", nil, "TOPLEFT", savedTargetFrameOffsetX, savedTargetFrameOffsetY)
 
-    Focus_Relock()
+    -- Relock.
+    Relock()
 end
+
+--
+--
+--
+
+-- TODO: Translate strings.
 
 -- Prompt during Focus repositioning.
 StaticPopupDialogs.FOCUS_SETUP = {
-    text = "Move your health around until you're satisfied with its position. Once you're done press Save. You can press Discard to undo any changes.",
+    text = "Drag your character's health around and your target's health will mirror its movement. Once you're done press Save. You can press Discard to undo any changes.",
     button1 = "Save",
     button2 = "Discard",
-    OnAccept = function() Focus_Apply() end,
-    OnCancel = function() Focus_Discard() end,
+    OnAccept = function() 
+        Apply();
+    end,
+    OnCancel = function() 
+        Discard();
+    end,
     timeout = 0,
     whileDead = 1,
     hideOnEscape = true,
-}
-
--- Prompt to reload the UI after reset.
-StaticPopupDialogs.FOCUS_RESET = {
-    text = "Would you like to reload your interface now?",
-    button1 = "Reload",
-    button2 = "Abort",
-    OnAccept = function() ReloadUI() end,
-    OnCancel = function() print("Your health's position will reset on your next login. You can type /reload to do it sooner.") end,
-    timeout = 0,
-    whileDead = 1,
-    hideOnEscape = true,
-}
+};
 
 -- Slash command setup.
-SLASH_FOCUS1 = "/focus"
-SlashCmdList.FOCUS = function(...)
-    if arg[1] == "reset" then
-        Focus_Reset()
+SLASH_FOCUS1 = "/focus";
+SlashCmdList.FOCUS = function(option)
+    if (FocusSavedVars.reset) then
+        Startup();
+    end
+
+    if (option == "reset") then
+        Reset();
     else
-        Focus_Unlock()
+        Unlock();
+    end
+end;
+
+function FocusFrame_OnLoad()
+    this:RegisterEvent("ADDON_LOADED");    
+end
+
+function FocusFrame_OnEvent()
+    if (event == "ADDON_LOADED") then
+        if (not FocusSavedVars) then
+            FocusSavedVars = {};
+        end
+
+        if (not FocusSavedVars.reset) then
+            Startup();
+        end
     end
 end
 
--- Entrypoint.
--- Focus_Setup()
+function FocusFrame_OnUpdate()
+    -- Whenever the player is dragging PlayerFrame around 
+    -- we do a calculation to mirror its movement across 
+    -- the Y axis and reposition TargetFrame accordingly.
+    if (isPlayerFrameMoving) then
+        local _, _, _, offsetX, offsetY = PlayerFrame:GetPoint(1);
+        local mirroredOffsetX = GetScreenWidth() - offsetX - TargetFrame:GetWidth();
+        TargetFrame:ClearAllPoints();
+        TargetFrame:SetPoint("TOPLEFT", nil, "TOPLEFT", mirroredOffsetX, offsetY);
+    end  
+end
--- Focus/README.md
+++ Focus/README.md
@@ -10,6 +10,8 @@
 
 Move the add-on directory `Focus` into `World of Warcraft\Interface\AddOns` before launching the game. After it's installed both your charater's health and your target's health are going to appear just above the center of the screen. Type `/focus` if you want to move them around. You can drag your character's health around and the target's health will mirror its movement across the vertical axis.
 
+Please note that **uninstalling the add-on won't restore your character's and target's health position**. That's because it's being handled by the _layout cache_ of the game. To **restore the default settings** use `/focus reset`.
+
 ## License
 
 The MIT License © 2018 Arthur Corenzan
