3a42828a901468a2fc3620168f94dee9bed94784
Arthur <arthur@corenzan.com>
2019-09-10 08:08:03 -0300

    Add percentage vallue over target's health bar

--- classic/Focused/Focused.lua
+++ classic/Focused/Focused.lua
@@ -54,3 +54,19 @@ function FocusedFrame_OnUpdate(self, elapsed)
 	end
 end
 
+--
+--
+--
+
+-- Bring back text label over target's health bar.
+-- Though only the percentage can be shown since the
+-- game's API doesn't provide the actual health values.
+--
+-- Also, it's not really a feature of this add-on but
+-- I've got no where else to put it.
+--
+TargetFrame.healthbar.Text = TargetFrameTextureFrame:CreateFontString("TargetFrameHealthBarText", "BACKGROUND", "TextStatusBarText");
+TargetFrame.healthbar.Text:SetPoint("CENTER", -50, 3);
+TargetFrame.healthbar.showPercentage = true;
+TargetFrame.healthbar.showNumeric = false;
+SetTextStatusBarText(TargetFrame.healthbar, TargetFrame.healthbar.Text);
