86ea2d0c392c0855e0949c5f39187f817118ba88
Arthur <arthur@corenzan.com>
2019-09-15 00:16:07 -0300

    Fix install.bat script

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,11 +2,20 @@
 
 set platform=%~1
 set source=%~dp0..\%~1
-set target=%~f2_%~1_\Interface\AddOns
+set target=%~f2\_%~1_\Interface\AddOns
 
 if "%platform%" == "" goto missing
-if not exist "%source%" goto invalid
-if not exist "%target%" goto invalid
+
+if not exist "%source%" (
+    set invalid=%source%
+    goto invalid
+)
+if not exist "%target%" (
+    set invalid=%target%
+    goto invalid
+)
+
+echo Installing add-ons from %source% to %target%
 
 for /f %%f in ('dir /b "%source%"') do (
     mklink /D "%target%\%%f" "%source%\%%f"
@@ -19,5 +28,5 @@ echo Missing argument. e.g. %0 classic "D:\World of Warcraft"
 exit /B 1
 
 :invalid
-echo Invalid argument.
+echo Invalid argument: %invalid%
 exit /B 1
