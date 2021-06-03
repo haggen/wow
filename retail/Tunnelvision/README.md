94df58e3d75a14e75dc21331a1ba0fccaed400ee
Arthur <arthur@corenzan.com>
2021-06-03 10:35:05 -0300

    Update retail to latest classic release (#85)

---
+++ retail/Tunnelvision/README.md
@@ -0,0 +1,31 @@
+# Tunnelvision
+
+> For people with tunnel vision.
+
+## About
+
+![Gif showing off the add-on in action](https://i.imgur.com/WHc1hCv.gif)
+
+**Tunnelvision** resets both your character's and target's frames position to the center of the screen and then ties them together so when you drag one the other follows in a symmetrical fashion. It's simple stuff but helps you keep an eye on resources and cooldowns during the most stressful moments and also with your OCD. ;)
+
+## Installation
+
+Download the latest version from the [Releases page](https://github.com/haggen/wow/releases) and extract it into `World of Warcraft\_classic_\Interface\AddOns` prior to launching the game.
+
+## Usage
+
+Upon logging in after the add-on's been installed the default position of your character's and target's frames will be set to the center of the screen. Their position and locking mechanism will be tied together so that whatever happens to one will also be applied to the other.
+
+If you have moved the frames prios to the installation of this add-on you might want to reset their position via the right-click menu.
+
+## Uninstall
+
+Simply delete the `Tunnelvision` folder from your add-ons directory.
+
+Please note that uninstalling the add-on won't restore your character's and target's frames position. For that you'll need to reset their position via the right-click menu and selecting Move Frame → Reset Position.
+
+## License
+
+The MIT License © 2018 Arthur Corenzan
+
+This Add-on is not created by, affiliated with or sponsored by Blizzard Entertainment, Inc. or its affiliates. The World of Warcraft® and related logos are registered trademarks or trademarks of Blizzard Entertainment, Inc. in the United States and/or other countries. All rights reserved.
---
+++ retail/Tunnelvision/Tunnelvision.lua
@@ -0,0 +1,108 @@
+-- Tunnelvision
+-- MIT License © 2018 Arthur Corenzan
+-- More on https://github.com/haggen/wow
+
+-- Add-on namespace.
+--
+local TUNNELVISION = ...;
+
+-- Default frame anchor points.
+--
+local defaultFramePoint = {
+	[PlayerFrame] = "RIGHT",
+	[TargetFrame] = "LEFT"
+};
+
+-- Reset frame position to the center of the screen.
+--
+local function SetInitialPosition(frame)
+	frame:SetUserPlaced(true);
+	frame:ClearAllPoints();
+	frame:SetPoint(defaultFramePoint[frame], UIParent, "CENTER");
+end
+
+-- Given a user placed frames, update its offsetX to a mirrored value of the given anchor.
+--
+local function SetMirroredPosition(frame, anchor)
+	local _, _, _, offsetX, offsetY = unpack(anchor);
+	local mirroredOffsetX = GetScreenWidth() - offsetX - frame:GetWidth();
+	frame:ClearAllPoints();
+	frame:SetPoint("TOPLEFT", UIParent, "TOPLEFT", mirroredOffsetX, offsetY);
+end
+
+--
+--
+--
+
+-- Saved variables.
+-- installed (boolean) - Tells if it's the first the add-on is being loaded.
+--
+TunnelvisionSavedVars = {
+	installed = false,
+};
+
+-- Main frame mixin.
+--
+TunnelvisionFrameMixin = {};
+
+-- Load callback.
+--
+function TunnelvisionFrameMixin:OnLoad()
+	self:RegisterEvent("ADDON_LOADED");
+end
+
+-- Registered events callback.
+--
+function TunnelvisionFrameMixin:OnEvent(event, ...)
+	if (event == "ADDON_LOADED") then
+		local name = ...;
+
+		if (name == TUNNELVISION) then
+			if (not TunnelvisionSavedVars.installed) then
+				SetInitialPosition(PlayerFrame);
+				PlayerFrame_SetLocked(false);
+				SetInitialPosition(TargetFrame);
+				TargetFrame_SetLocked(false);
+			end
+			TunnelvisionSavedVars.installed = true;
+		end
+	end
+end
+
+-- Update the other frame's position whenever one is being dragged.
+--
+function TunnelvisionFrameMixin:OnUpdate()
+	if (PlayerFrame:IsDragging()) then
+		SetMirroredPosition(TargetFrame, {PlayerFrame:GetPoint(1)});
+	elseif (TargetFrame:IsDragging()) then
+		SetMirroredPosition(PlayerFrame, {TargetFrame:GetPoint(1)});
+	end
+end
+
+--
+--
+--
+
+-- Hook into frame options.
+--
+hooksecurefunc("TargetFrame_ResetUserPlacedPosition", function()
+	SetInitialPosition(TargetFrame);
+	SetInitialPosition(PlayerFrame);
+end);
+
+hooksecurefunc("TargetFrame_SetLocked", function(locked)
+	if (PLAYER_FRAME_UNLOCKED == locked) then
+		PlayerFrame_SetLocked(locked);
+	end
+end);
+
+hooksecurefunc("PlayerFrame_ResetUserPlacedPosition", function()
+	SetInitialPosition(TargetFrame);
+	SetInitialPosition(PlayerFrame);
+end);
+
+hooksecurefunc("PlayerFrame_SetLocked", function(locked)
+	if (TARGET_FRAME_UNLOCKED == locked) then
+		TargetFrame_SetLocked(locked);
+	end
+end);
